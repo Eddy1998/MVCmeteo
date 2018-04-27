326d8b8900306df9cbd9bd1d7bec65f4a387e3be
eddyalbines <eddyalbines@gmail.com>
2018-04-27 14:15:36 -0400

    .

--- controller.js
+++ controller.js
@@ -3,10 +3,56 @@ $( document ).ready(function() {
   
   $("#search").click(function (){
     console.log("bottone click");
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather",{ "q":$("#localita").val(),"APPID":"3b8939ce1f3413f183f908d321debab5"} , function(result){
+    var d= new Date();
+    d.toUTCString();
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather",{ "q":$("#localita").val(),"APPID":"3b8939ce1f3413f183f908d321debab5","units":"metric"} , function(result){
       console.log(result);
-    var string="<table><tr><th>Main</th><td>"+result.weather[0].main+"</td></tr><tr><th>Description</th><td>"+result.weather[0].description+"</td></tr><tr><th>Temp</th><td>"+result.main.temp+"</td></tr></table>";
-    $("#ciao").append(string);
+   
+      var primo=d+"<br><table><tr><th scope='col' colspan='2'>"+result.name+", "+result.sys.country+" (lon= "+result.coord.lon+", lat= "+result.coord.lat+")"+"<th></tr>";
+    var secondo="<tr><th>Main</th><td>"+result.weather[0].main+"</td></tr>";
+      var ter="<tr><th>Description</th><td>"+result.weather[0].description+"</td></tr>";
+      var quar="<tr><th>Temp</th><td>"+result.main.temp+" &#x2103;</td></tr>";
+      var quin="<tr><th>Pressure</th><td>"+result.main.pressure+" hPa</td></tr>";
+      var sest="<tr><th>Humidity</th><td>"+result.main.humidity+"%</td></tr>";
+      var sett="<tr><th>Wind speed</th><td>"+result.wind.speed+" m/s</td></tr>";
+      var ott="<tr><th>Wind deg</th><td>"+result.wind.deg+"</td></tr>";
+      var nov="<tr><th>Visibility</th><td>"+result.visibility+"</td></tr></table>";
+      var string=primo+secondo+ter+quar+quin+sest+sett+ott+nov;
+      $("#first").append(string);
+    });
+  }); 
+  $("#search1").click(function (){
+    console.log("bottone click");
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast",{ "q":$("#localita1").val(),"APPID":"3b8939ce1f3413f183f908d321debab5","units":"metric"} , function(result){
+      console.log(result);
+      var d=result.name+", "+result.country+" (lon= "+result.city.coord.lon+", lat= "+result.city.coord.lat+")";
+     //$('#select');
+      var primo="<br><table><tr><th scope='col' colspan='2'>Select a date/time <select id='myselect'></select><th></tr></table>";
+       //$(result).each(function(i, o) {
+         //   $('#select').append($('<option>').text(o.list[i].dt_txt).attr('value', i));
+       // });
+           //funct $(result).each(function(i, o) {
+            //var $option = $('<option>').text(o.list[i].dt_txt).attr('value', i);
+            //$('#select').append($option);
+             // });
+      $(result.list).each(function(i,o) {
+        console.log(o.dt_txt);
+        $('#set').append($('<option>', { 
+        value: i,
+        text : o.dt_txt,
+    }));
+
+      });
+//         var secondo="<tr><th>Main</th><td>"+result.weather[0].main+"</td></tr>";
+//         var ter="<tr><th>Description</th><td>"+result.weather[0].description+"</td></tr>";
+//         var quar="<tr><th>Temp</th><td>"+result.main.temp+" &#x2103;</td></tr>";
+//         var quin="<tr><th>Pressure</th><td>"+result.main.pressure+" hPa</td></tr>";
+//         var sest="<tr><th>Humidity</th><td>"+result.main.humidity+"%</td></tr>";
+//         var sett="<tr><th>Wind speed</th><td>"+result.wind.speed+" m/s</td></tr>";
+//         var ott="<tr><th>Wind deg</th><td>"+result.wind.deg+"</td></tr>";
+//         var nov="<tr><th>Visibility</th><td>"+result.visibility+"</td></tr></table>";
+//         var string=primo+secondo+ter+quar+quin+sest+sett+ott+nov;
+        
     });
   }); 
 });
--- ind.php
+++ ind.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html >
 <head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -7,26 +8,39 @@
 </head>
 <body>
     
-  </div>
   <div class="row">
-  <div id="ciao" class="col-sm-4">
- 
-  </div>
-    <div class="col-sm-4" class="panel panel-default" position="center" >
+ <div class="col-sm-6">
+    <div class="col-sm-6" class="panel panel-default" position="center" >
      <div class="form-group">
         <legend class="control-label">Inserisci localita'</legend>
         <div>
           <input id="localita" type="text" class="form-control" placeholder="place" >
         </div>
-    </div>
+      </div>
       <button id="search"  type="submit" class="btn btn-info"  ><b>Search</b></button>  
-    </div>
+   
+      <div id="first">
 
-  
-  
-    <div  class="col-sm-4">
-      
+      </div>
+   </div>
   </div>
+  
+    <div  class="col-sm-6">
+         <div class="col-sm-6" class="panel panel-default" position="center" >
+           <div class="form-group">
+              <legend class="control-label">Inserisci localita'</legend>
+              <div>
+                <input id="localita1" type="text" class="form-control" placeholder="place" >
+              </div>
+            </div>
+            <button id="search1"  type="submit" class="btn btn-danger"  ><b>Search</b></button>  
+         </div>
+      <div id="second">
+        <select id="set">
+          
+        </select>
+      </div>
+    </div>
   </div>
   
 </body>
