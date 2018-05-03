e76af0233578166cf8ed05d96d073dcf78220ff1
eddyalbines <eddyalbines@gmail.com>
2018-05-03 08:21:00 -0400

    .

--- controller.js
+++ controller.js
@@ -1,83 +1,91 @@
+$(document).ready(function() {
+  console.log("new");
 
-$( document ).ready(function() {
-  
-  $("#search").click(function (){
+  $("#search").click(function() {
     console.log("bottone click");
     $('#first').empty();
-    var d= new Date();
+    var d = new Date();
     d.toUTCString();
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather",{ "q":$("#localita").val(),"APPID":"3b8939ce1f3413f183f908d321debab5","units":"metric"} , function(result){
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather", {
+      "q": $("#localita").val(),
+      "APPID": "3b8939ce1f3413f183f908d321debab5",
+      "units": "metric"
+    }, function(result) {
       console.log(result);
-      var cit="<h2 class='fh5co-sub-ddown'>"+result.name+", "+result.sys.country+"</h2>";
-       var k= "<br><img src='http://openweathermap.org/img/w/"+result.weather[0].icon+".png' style='width: 200px;  height: 200px;'><br>";
-      var primo="<br><table><tr><th scope='col' colspan='2'>"+result.name+", "+result.sys.country+" (lon= "+result.coord.lon+", lat= "+result.coord.lat+") </th></tr>";
-    var secondo="<tr><th>Main</th><td>"+result.weather[0].main+"</td></tr>";
-      var ter="<tr><th>Description</th><td>"+result.weather[0].description+"</td></tr>";
-      var quar="<tr><th>Temp</th><td>"+result.main.temp+" &#x2103;</td></tr>";
-      var quin="<tr><th>Pressure</th><td>"+result.main.pressure+" hPa</td></tr>";
-      var sest="<tr><th>Humidity</th><td>"+result.main.humidity+"%</td></tr>";
-      var sett="<tr><th>Wind speed</th><td>"+result.wind.speed+" m/s</td></tr>";
-      var ott="<tr><th>Wind deg</th><td>"+result.wind.deg+"</td></tr>";
-      var nov="<tr><th>Visibility</th><td>"+result.visibility+"</td></tr></table>";
-      var string=cit+d+k+primo+secondo+ter+quar+quin+sest+sett+ott+nov;
+      var cit = "<h2 class='fh5co-sub-ddown'>" + result.name + ", " + result.sys.country + "</h2>";
+      var k = "<br><img src='http://openweathermap.org/img/w/" + result.weather[0].icon + ".png' style='width: 200px;  height: 200px;'><br>";
+      var primo = "<br><table><tr><th scope='col' colspan='2'>" + result.name + ", " + result.sys.country + " (lon= " + result.coord.lon + ", lat= " + result.coord.lat + ") </th></tr>";
+      var secondo = "<tr><th>Main</th><td>" + result.weather[0].main + "</td></tr>";
+      var ter = "<tr><th>Description</th><td>" + result.weather[0].description + "</td></tr>";
+      var quar = "<tr><th>Temp</th><td>" + result.main.temp + " &#x2103;</td></tr>";
+      var quin = "<tr><th>Pressure</th><td>" + result.main.pressure + " hPa</td></tr>";
+      var sest = "<tr><th>Humidity</th><td>" + result.main.humidity + "%</td></tr>";
+      var sett = "<tr><th>Wind speed</th><td>" + result.wind.speed + " m/s</td></tr>";
+      var ott = "<tr><th>Wind deg</th><td>" + result.wind.deg + "</td></tr></table>";
+      var string = cit + d + k + primo + secondo + ter + quar + quin + sest + sett + ott;
       $("#first").append(string);
-      $("#first").append("<i class='owf owf-"+result.weather[0].id+"></i>");
-    });
-  }); 
-  $("#search1").click(function (){
-    console.log("bottone click");
-      $.getJSON("https://api.openweathermap.org/data/2.5/forecast",{ "q":$("#localita1").val(),"APPID":"3b8939ce1f3413f183f908d321debab5","units":"metric"} , function(risposta){
-      console.log(risposta);
-      
-      $('#primeiro').empty();
-         var d="<h2 class='fh5co-sub-ddown'>"+result.name+", "+result.sys.country+"</h2>";
-      //var d = risposta.city.name+", "+risposta.city.country+" (lon= "+risposta.city.coord.lon+", lat= "+risposta.city.coord.lat+")<br>Select a date/time  <select id='set'>  </select>";
-      $('#primeiro').append(d);
-        console.log("ciao");
-     
-      $(risposta.list).each(function(i,o) {
-        
-        $('#set').append($('<option>', { 
-        value: i,
-        text : o.dt_txt,}));
-        });
-      var a = $('#set').val();
-       $('#second').empty();
-      var primo="<br><table><tr><th scope='col' colspan='2'><img src='http://openweathermap.org/img/w/"+risposta.list[a].weather[0].icon+".png'></th></tr>";
-      var secondo="<tr><th>Main</th><td>"+risposta.list[a].weather[0].main+"</td></tr>";
-        var ter="<tr><th>Description</th><td>"+risposta.list[a].weather[0].description+"</td></tr>";
-        var quar="<tr><th>Temp</th><td>"+risposta.list[a].main.temp+" &#x2103;</td></tr>";
-         var quin="<tr><th>Pressure</th><td>"+risposta.list[a].main.pressure+" hPa</td></tr>";
-         var sest="<tr><th>Humidity</th><td>"+risposta.list[a].main.humidity+"%</td></tr>";
-        var sett="<tr><th>Wind speed</th><td>"+risposta.list[a].wind.speed+" m/s</td></tr>";
-         var ott="<tr><th>Wind deg</th><td>"+risposta.list[a].wind.deg+"</td></tr></table>";
-      var string=primo+secondo+ter+quar+quin+sest+sett+ott;
-      $('#second').append(string);
-      $('#set').change(function(){
-        var a = $('#set').val();
-         $('#second').empty();
-        var primo="<table><tr><th scope='col' colspan='2'><img src='http://openweathermap.org/img/w/"+risposta.list[a].weather[0].icon+".png'></th></tr>";
-        var secondo="<br><table><tr><th>Main</th><td>"+risposta.list[a].weather[0].main+"</td></tr>";
-        var ter="<tr><th>Description</th><td>"+risposta.list[a].weather[0].description+"</td></tr>";
-        var quar="<tr><th>Temp</th><td>"+risposta.list[a].main.temp+" &#x2103;</td></tr>";
-         var quin="<tr><th>Pressure</th><td>"+risposta.list[a].main.pressure+" hPa</td></tr>";
-         var sest="<tr><th>Humidity</th><td>"+risposta.list[a].main.humidity+"%</td></tr>";
-        var sett="<tr><th>Wind speed</th><td>"+risposta.list[a].wind.speed+" m/s</td></tr>";
-         var ott="<tr><th>Wind deg</th><td>"+risposta.list[a].wind.deg+"</td></tr></table>";
-      var string=primo+secondo+ter+quar+quin+sest+sett+ott;
-      $('#second').append(string);
-
-      });
-       
-     
-        
+      $("#first").append("<i class='owf owf-" + result.weather[0].id + "></i>");
     });
   });
-  $( "#set" ).change(function() {
-   
-  changed($('#set').val());
-});
-  $(function changed(a,result){
+  $("#search1").click(function() {
+    console.log("bottone click");
+    $.getJSON("https://api.openweathermap.org/data/2.5/forecast", {
+      "q": $("#localita1").val(),
+      "APPID": "3b8939ce1f3413f183f908d321debab5",
+      "units": "metric"
+    }, function(risposta) {
+      console.log(risposta);
 
-}); 
+      $('#primeiro').empty();
+      var d = "<h2 class='fh5co-sub-ddown'>" + result.name + ", " + result.sys.country + "</h2>";
+      //var d = risposta.city.name+", "+risposta.city.country+" (lon= "+risposta.city.coord.lon+", lat= "+risposta.city.coord.lat+")<br>Select a date/time  <select id='set'>  </select>";
+      $('#primeiro').append(d);
+      console.log("ciao");
+
+      $(risposta.list).each(function(i, o) {
+
+        $('#set').append($('<option>', {
+          value: i,
+          text: o.dt_txt,
+        }));
+      });
+      var a = $('#set').val();
+      $('#second').empty();
+      var primo = "<br><table><tr><th scope='col' colspan='2'><img src='http://openweathermap.org/img/w/" + risposta.list[a].weather[0].icon + ".png'></th></tr>";
+      var secondo = "<tr><th>Main</th><td>" + risposta.list[a].weather[0].main + "</td></tr>";
+      var ter = "<tr><th>Description</th><td>" + risposta.list[a].weather[0].description + "</td></tr>";
+      var quar = "<tr><th>Temp</th><td>" + risposta.list[a].main.temp + " &#x2103;</td></tr>";
+      var quin = "<tr><th>Pressure</th><td>" + risposta.list[a].main.pressure + " hPa</td></tr>";
+      var sest = "<tr><th>Humidity</th><td>" + risposta.list[a].main.humidity + "%</td></tr>";
+      var sett = "<tr><th>Wind speed</th><td>" + risposta.list[a].wind.speed + " m/s</td></tr>";
+      var ott = "<tr><th>Wind deg</th><td>" + risposta.list[a].wind.deg + "</td></tr></table>";
+      var string = primo + secondo + ter + quar + quin + sest + sett + ott;
+      $('#second').append(string);
+      $('#set').change(function() {
+        var a = $('#set').val();
+        $('#second').empty();
+        var primo = "<table><tr><th scope='col' colspan='2'><img src='http://openweathermap.org/img/w/" + risposta.list[a].weather[0].icon + ".png'></th></tr>";
+        var secondo = "<br><table><tr><th>Main</th><td>" + risposta.list[a].weather[0].main + "</td></tr>";
+        var ter = "<tr><th>Description</th><td>" + risposta.list[a].weather[0].description + "</td></tr>";
+        var quar = "<tr><th>Temp</th><td>" + risposta.list[a].main.temp + " &#x2103;</td></tr>";
+        var quin = "<tr><th>Pressure</th><td>" + risposta.list[a].main.pressure + " hPa</td></tr>";
+        var sest = "<tr><th>Humidity</th><td>" + risposta.list[a].main.humidity + "%</td></tr>";
+        var sett = "<tr><th>Wind speed</th><td>" + risposta.list[a].wind.speed + " m/s</td></tr>";
+        var ott = "<tr><th>Wind deg</th><td>" + risposta.list[a].wind.deg + "</td></tr></table>";
+        var string = primo + secondo + ter + quar + quin + sest + sett + ott;
+        $('#second').append(string);
+
+      });
+
+
+
+    });
+  });
+  $("#set").change(function() {
+
+    changed($('#set').val());
+  });
+  $(function changed(a, result) {
+
+  });
 });
--- index.php
+++ index.php
@@ -1,118 +1,120 @@
-
 <!DOCTYPE html>
- <html class="no-js"> <!--<![endif]-->
-	<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="  crossorigin="anonymous"></script>
-  
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>todayCloud</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>todayCloud</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" type="image/x-icon" href="today.ico" />
-    <link href="css/owfont-regular.css" rel="stylesheet" type="text/css">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'> 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<script src="controller.js"></script>
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Superfish -->
-	<link rel="stylesheet" href="css/superfish.css">
+  <link href="css/owfont-regular.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <script src="controller.js"></script>
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css/icomoon.css">
+  <!-- Bootstrap  -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <!-- Superfish -->
+  <link rel="stylesheet" href="css/superfish.css">
 
-	<link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 
 
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
+  <!-- Modernizr JS -->
+  <script src="js/modernizr-2.6.2.min.js"></script>
 
-	</head>
-	<body>
-		<div id="fh5co-wrapper">
-		<div id="fh5co-page">
-		<div id="fh5co-header" >
-			<header id="fh5co-header-section">
-				<div class="container">
-					<div class="nav-header" style="height: 105px;">
-						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-						<h1 id="fh5co-logo" style="color:#ffffff";>todaycloud <img src="img.png" style="width: 50px;"></h1>
-						<!-- START #fh5co-menu-wrap -->
-						<nav id="fh5co-menu-wrap" role="navigation">
-							<ul class="sf-menu" id="fh5co-primary-menu">
-								<li >
-									<a href="index.php" class="fh5co-sub-ddown">Current Weather</a>
-									
-								</li>
-								<li>
-									<a href="index1.php" class="fh5co-sub-ddown">Forecast</a>
-									
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</div>
-			</header>
-			
-		</div>
-		
+</head>
 
-		<div class="fh5co-hero">
-			<div class="fh5co-overlay"></div>
-			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" "=" style="background-position: 0px 0px; height: 296px;">
-				
-				
-				<div class="desc animate-box fadeInUp animated" style="margin-top: 0px;top: 140px;">
-				
-          <h2 class="fh5co-sub-ddown">Current <strong>Weather</strong> <br>in your city </h2>
+<body>
+  <div id="fh5co-wrapper">
+    <div id="fh5co-page">
+      <div id="fh5co-header">
+        <header id="fh5co-header-section">
+          <div class="container">
+            <div class="nav-header" style="height: 105px;">
+              <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+              <h1 id="fh5co-logo" style="color:#ffffff" ;>todaycloud <img src="img.png" style="width: 50px;"></h1>
+              <!-- START #fh5co-menu-wrap -->
+              <nav id="fh5co-menu-wrap" role="navigation">
+                <ul class="sf-menu" id="fh5co-primary-menu">
+                  <li>
+                    <a href="index.php" class="fh5co-sub-ddown" >Current Weather</a>
+
+                  </li>
+                  <li>
+                    <a href="index1.php" class="fh5co-sub-ddown">Forecast</a>
+
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+
+      </div>
+
+
+      <div class="fh5co-hero">
+        <div class="fh5co-overlay"></div>
+        <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" "=" style="background-position: 0px 0px; height: 296px;">
+
+
+          <div class="desc animate-box fadeInUp animated" style="margin-top: 0px;top: 140px;">
+
+            <h2 class="fh5co-sub-ddown">Current <strong>Weather</strong> <br>in your city </h2>
             <div class="row">
-                <div class="col-sm-4">
-                
-                </div>
-                <div class="col-sm-4">
+              <div class="col-sm-4">
 
-                    <span><input id="localita" type="text" class="form-control" placeholder="Insert your city" ></span>
-                    <span><a id="search" class="btn btn-primary btn-lg" href="#" >Search</a></span>
-                  <div id="first" class="row">
+              </div>
+              <div class="col-sm-4">
 
-                  </div>
+                <span><input id="localita" type="text" class="form-control" placeholder="Insert your city" ></span>
+                <span><a id="search" class="btn btn-primary btn-lg" href="#" >Search</a></span>
+                <div id="first" class="row">
+
                 </div>
-                <div class="col-sm-4">
-                
-                </div>
+              </div>
+              <div class="col-sm-4">
+
+              </div>
+            </div>
+
           </div>
-          
-          </div>
-			</div>
-			</div>
+        </div>
+      </div>
 
-		</div>
-
-	
-
-	</div>
-	</div>
+    </div>
 
 
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Stellar -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Superfish -->
-	<script src="js/hoverIntent.js"></script>
-	<script src="js/superfish.js"></script>
 
-	<!-- Main JS -->
-	<script src="js/main.js"></script>
+  </div>
+  </div>
 
-	</body>
+
+  <script src="js/jquery.min.js"></script>
+  <!-- jQuery Easing -->
+  <script src="js/jquery.easing.1.3.js"></script>
+  <!-- Bootstrap -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- Waypoints -->
+  <script src="js/jquery.waypoints.min.js"></script>
+  <!-- Stellar -->
+  <script src="js/jquery.stellar.min.js"></script>
+  <!-- Superfish -->
+  <script src="js/hoverIntent.js"></script>
+  <script src="js/superfish.js"></script>
+
+  <!-- Main JS -->
+  <script src="js/main.js"></script>
+
+</body>
+
 </html>
-
